2f7c11afaea8730f4e4da96a2b1a8d7f9e5549f8
gengarka <gengarka@student.21-school.ru>
2023-02-23 13:36:52 +0300

    s21_determinant fix 1

--- src/main.c
+++ src/main.c
@@ -10,28 +10,14 @@ void s21_init_matrix(double number, matrix_t *A) {
 int main() {
 
 
-    matrix_t A = {0};
-    matrix_t B = {0};
-    matrix_t C = {0};
-    s21_create_matrix(3, 2, &A);
-    s21_create_matrix(2, 3, &C);
-    s21_init_matrix(1.0, &A);
-    C.matrix[0][0] = 1;
-    C.matrix[0][1] = 3;
-    C.matrix[0][2] = 5;
-    C.matrix[1][0] = 2;
-    C.matrix[1][1] = 4;
-    C.matrix[1][2] = 6;
+    int res = 0;
+    double determ = 0.0;
+    matrix_t A = {NULL, 0, 0};
 
-    s21_transpose(&A, &B);
 
-    s21_eq_matrix(&C, &B);
-
-//    ck_assert_int_eq(s21_transpose(&A, &B), 0);
-//    ck_assert_int_eq(s21_eq_matrix(&C, &B), 1);
+    res = s21_determinant(&A, &determ);
+//    ck_assert_int_eq(res, ERROR_INIT);
     s21_remove_matrix(&A);
-    s21_remove_matrix(&B);
-    s21_remove_matrix(&C);
 
 
 
--- src/s21_determinant.c
+++ src/s21_determinant.c
@@ -5,27 +5,32 @@ int s21_determinant(matrix_t *A, double *result) {
     int degree = 1;
     int status = OK;
 
-    if (A->rows == 1) {
-        *result = A->matrix[0][0];
-        return status;
-    } else if (A->rows == 2) {
-        *result = A->matrix[0][0] * A->matrix[1][1] - A->matrix[1][0] * A->matrix[0][1];
-        return status;
-    } else {
-        double lasting_result = 0;
-        for (int i = 0; i < A->columns; i++) {
-            matrix_t crop_matrix;
-            cut_matrix(0, i, A, &crop_matrix);
-
-
-            s21_determinant(&crop_matrix, result);
-
-            lasting_result += *result * A->matrix[0][i] * degree;
-            degree *= -1;
-
-        }
-        *result = lasting_result;
+    if (!is_matrix_correct(A)) {
+        status = INCORRECT_M;
     }
+
+    if (status == OK) {
+        if (A->rows == 1) {
+            *result = A->matrix[0][0];
+        } else if (A->rows == 2) {
+            *result = A->matrix[0][0] * A->matrix[1][1] - A->matrix[1][0] * A->matrix[0][1];
+        } else {
+            double lasting_result = 0;
+            for (int i = 0; i < A->columns; i++) {
+                matrix_t crop_matrix;
+                cut_matrix(0, i, A, &crop_matrix);
+
+
+                s21_determinant(&crop_matrix, result);
+
+                lasting_result += *result * A->matrix[0][i] * degree;
+                degree *= -1;
+
+            }
+            *result = lasting_result;
+        }
+    }
+
 
     return status;
 }
